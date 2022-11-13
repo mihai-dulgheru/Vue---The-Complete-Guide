2df6c084835bad1521940ab19c568473a77a7336
mihai-dulgheru <63505058+mihai-dulgheru@users.noreply.github.com>
2022-11-14 00:06:51 +0200

    add resources

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,59 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: "",
+      message: "Vue is great!",
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+});
+
+app.mount("#app");
+
+// ...
+
+// let message = "Hello!";
+
+// let longMessage = message + " World!";
+
+// console.log(longMessage);
+
+// message = "Hello!!!";
+
+// console.log(longMessage);
+
+const data = {
+  message: "Hello!",
+  longMessage: "Hello! World!",
+};
+
+const handler = {
+  set(target, key, value) {
+    if (key === "message") {
+      target.longMessage = value + " World!";
+    }
+    target.message = value;
+  },
+};
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = "Hello!!!";
+console.log(proxy.longMessage);
+
+const app2 = Vue.createApp({
+  data() {
+    return {
+      favoriteMeal: "Pizza",
+    };
+  },
+});
+
+app2.mount("#app2");
