d5ea675884178cabc1cdc2edd401fe6409afc5af
Mihai-Nicolae Dulgheru <dulgherumihai19@stud.ase.ro>
2022-12-05 22:09:17 +0200

    add scoped slots

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -8,11 +8,18 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals #default="slotProps">
+      <!-- <template #default="slotProps"> -->
+      <h2>{{ slotProps.item }}</h2>
+      <p>{{ slotProps.anotherProp }}</p>
+      <!-- </template> -->
+    </course-goals>
   </div>
 </template>
 
 <script>
 import BadgeList from "./components/BadgeList.vue";
+import CourseGoals from "./components/CourseGoals.vue";
 import TheHeader from "./components/TheHeader.vue";
 import UserInfo from "./components/UserInfo.vue";
 
@@ -21,6 +28,7 @@ export default {
     TheHeader,
     BadgeList,
     UserInfo,
+    CourseGoals,
   },
   data() {
     return {
